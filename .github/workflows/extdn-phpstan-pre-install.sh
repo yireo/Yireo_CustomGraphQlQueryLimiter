cd75ed1391f5725450791574739106ef85e99e68
jisse Reitsma <jisse@yireo.com>
2022-02-09 17:01:32 +0100

    Add preinstall for PHPStan to add integration test helper

---
+++ .github/workflows/extdn-phpstan-pre-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+composer config minimum-stability dev
+composer config prefer-stable false
+
+composer require yireo/magento2-integration-test-helper:@dev --no-update
+composer require yireo/magento2-replace-bundled:^3.0 --no-update
+composer require yireo/magento2-replace-inventory:^3.0 --no-update
