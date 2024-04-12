5103c2860a9d352bb13029aba7595eb5dccdd1f0
Jisse Reitsma <jisse@yireo.com>
2024-04-12 16:07:10 +0200

    PHP 8.2 upgrades

--- .github/workflows/extdn-phpstan-pre-install.sh
+++ .github/workflows/extdn-phpstan-pre-install.sh
@@ -3,5 +3,5 @@ composer config minimum-stability dev
 composer config prefer-stable false
 
 composer require yireo/magento2-integration-test-helper:~0.0.1 --no-update
-composer require yireo/magento2-replace-bundled:^4.0 --no-update
-composer require yireo/magento2-replace-inventory:^4.0 --no-update
+#composer require yireo/magento2-replace-bundled:^4.0 --no-update
+#composer require yireo/magento2-replace-inventory:^4.0 --no-update
